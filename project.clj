88fcf1eb37216725290311099d3c08c7b26e8b19
Jesus Diez <jesus.diez@akamon.com>
2015-03-03 02:11:51 +0100

    fix project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clonciliate "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Management of bank remittance files to conciliate payments"
+  :url "http://github.com/jesusdiez/clonciliate"
+  :license {:name "GPLv2.0"
+            :url "http://www.gnu.org/licenses/gpl-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot clonciliate.core
   :target-path "target/%s"
